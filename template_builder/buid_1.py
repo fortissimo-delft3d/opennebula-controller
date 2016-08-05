f7854d840ee3e89fff4f89908dc699e6787b6002
Lykle Voort <lykle.voort@surfsara.nl>
2016-08-05 14:13:54 +0200

    extra elements added to template xml

--- template_builder/buid_1.py
+++ template_builder/buid_1.py
@@ -11,7 +11,25 @@ SubElement(context, "NETWORK")       .text = "YES"
 SubElement(context, "SSH_PUBLIC_KEY").text = "$USER[SSH_PUBLIC_KEY]"
 
 # CPU
-cpu = Subelement(template, "
+SubElement(template, "CPU")          .text = "1"
 
-print dump(template)
+# DISK
+disk = SubElement(template, "DISK")
+SubElement(disk, "IMAGE")            .text = "delf3d-worker-os"
+SubElement(disk, "IMAGE_UNAME")      .text = "fortissimo-lyklev"
 
+# FEATURES
+features = SubElement(template, "FEATURES")
+SubElement(features, "ACPI")         .text = "yes"
+SubElement(features, "LOCALTIME")    .localtime = "no"
+
+# GRAPHICS
+graphics = SubElement(template, "GRAPHICS")
+SubElement(graphics, "LISTEN")       .text = "0.0.0.0"
+SubElement(graphics, "TYPE")         .text = 'vnc'
+
+
+
+
+
+dump(template)
