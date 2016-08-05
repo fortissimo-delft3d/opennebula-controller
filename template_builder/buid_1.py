07a05acb4344cee64fe18bb00b5298881a50a72e
Lykle Voort <lykle.voort@surfsara.nl>
2016-08-05 15:56:19 +0200

    Completed template

--- template_builder/buid_1.py
+++ template_builder/buid_1.py
@@ -5,31 +5,50 @@ from xml.etree.ElementTree import Element, SubElement, dump
 
 template = Element("template")
 
-# context
-context = SubElement(template, "context")
-SubElement(context, "NETWORK")       .text = "YES"
-SubElement(context, "SSH_PUBLIC_KEY").text = "$USER[SSH_PUBLIC_KEY]"
 
 # CPU
 SubElement(template, "CPU")          .text = "1"
+
+# VCPU
+SubElement(template, "VCPU")         .text = "1"
+
+
+# MEMORY
+SubElement(template, "MEMORY")       .text = "2048"
 
 # DISK
 disk = SubElement(template, "DISK")
 SubElement(disk, "IMAGE")            .text = "delf3d-worker-os"
 SubElement(disk, "IMAGE_UNAME")      .text = "fortissimo-lyklev"
 
+# os
+os = SubElement(template, "OS")
+SubElement(os, "ARCH")               .text = "x86_64"
+
+# context
+context = SubElement(template, "context")
+SubElement(context, "NETWORK")       .text = "YES"
+SubElement(context, "SSH_PUBLIC_KEY").text = "$USER[SSH_PUBLIC_KEY]"
+
+# NIC (Network interfaces)
+nic = SubElement(template, "NIC")
+nic_0 = SubElement(nic, "0")
+SubElement(nic_0, "NETWORK")         .text = "internet"
+SubElement(nic_0, "NETWORK_UNAME")   .text = "oneadmin"
+
+nic_1 = SubElement(nic, "1")
+SubElement(nic_0, "NETWORK")         .text = "fortissimo-surfsara.int"
+SubElement(nic_0, "NETWORK_UNAME")   .text = "fortissimo-lyklev"
+
+
 # FEATURES
 features = SubElement(template, "FEATURES")
 SubElement(features, "ACPI")         .text = "yes"
-SubElement(features, "LOCALTIME")    .localtime = "no"
+SubElement(features, "LOCALTIME")    .text = "no"
 
 # GRAPHICS
 graphics = SubElement(template, "GRAPHICS")
 SubElement(graphics, "LISTEN")       .text = "0.0.0.0"
-SubElement(graphics, "TYPE")         .text = 'vnc'
-
-
-
-
+SubElement(graphics, "TYPE")         .text = "vnc"
 
 dump(template)
